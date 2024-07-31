7fb97cbd7bdf151c3f72cb105abc7b32aade89e6
Andy Wang <yagosys@gmail.com>
2024-07-31 14:09:22 +0800

    init

--- fortiweb/enablewaf.sh
+++ fortiweb/enablewaf.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
-podname=$(kubectl get pods -l app=fortiweb -n fortiweb -o=jsonpath='{.items[*].metadata.name}')
-kubectl exec -i po/$podname -n fortiweb -- /bin/cli admin console <<EOF
+
+# Get all pod names matching the label app=fortiweb in the fortiweb namespace
+podnames=$(kubectl get pods -l app=fortiweb -n fortiweb -o=jsonpath='{.items[*].metadata.name}')
+
+# Iterate over each pod name
+for podname in $podnames; do
+  kubectl exec -i po/$podname -n fortiweb -- /bin/cli admin console <<EOF
 config server-policy policy
 edit "fortiwebingressrulepolicy"
 set web-protection-profile "Inline Extended Protection"
 end
 EOF
+done
 
